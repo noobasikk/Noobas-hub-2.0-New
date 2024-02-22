4053db5e67d0f638c971ff25c9631b749fe231da
noobasikk <156350945+noobasikk@users.noreply.github.com>
2024-02-22 18:09:05 +0300

    Update Person.lua

--- Ayo/Noobas/Hub/Got/Updated/Yay/noobasikk/Russian/Person.lua
+++ Ayo/Noobas/Hub/Got/Updated/Yay/noobasikk/Russian/Person.lua
@@ -1,3 +1,3 @@
 local PlayersInServer = game.Players.LocalPlayer
 
-PlayersInServer:Kick("Noobas Hub 2.0 Сейчас Обновляется, Ожидайте/Noobas Hub 2.0 Is In Udate Please Wait!")
+PlayersInServer:Kick("Noobas Hub 2.0 Сейчас Обновляется, Ожидайте/Noobas Hub 2.0 Is In Update Please Wait!")
